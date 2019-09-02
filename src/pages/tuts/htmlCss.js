241ce7c56e9f46c4da0014056c84f3d909a7e35a
IOT-Arduino <bunkajin@icloud.com>
2019-09-02 10:29:42 +0900

    pages/tuts -> components/PageGrid.js

--- src/pages/tuts/htmlCss.js
+++ src/pages/tuts/htmlCss.js
@@ -8,13 +8,7 @@ import TutsHeader from '../../components/TutsHeader'
 import styles from './styles/tutorial.module.scss'
 
 import Grid from '@material-ui/core/Grid'
-import Card from '@material-ui/core/Card'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
-import Button from '@material-ui/core/Button'
-import Typography from '@material-ui/core/Typography'
-
+import PageGrid from '../../components/PageGrid'
 
 class htmlCss extends React.Component {
 
@@ -54,7 +48,7 @@ class htmlCss extends React.Component {
 
 
       return (
-            <Layout>
+        <Layout>
             <Head title="CSS&jQuery Tutorial Selection" />
             
             <div className={styles.container}>
@@ -62,15 +56,16 @@ class htmlCss extends React.Component {
             <TutsHeader />
 
             <div className={styles.selectBlock}>
-            <Select
-            value={selectedOption}
-            onChange={this.handleChange}
-            options={selectValue}
-            className={styles.selector}
-            />
-            <button onClick={this.handleReset}
-            className={styles.resetBtn}>Reset</button>
-        </div>
+              <Select
+                value={selectedOption}
+                onChange={this.handleChange}
+                options={selectValue}
+                className={styles.selector}
+              />
+              <button onClick={this.handleReset}
+                className={styles.resetBtn}>Reset
+              </button>
+            </div>
 
         <Grid container>
         {data.allAirtable.edges
@@ -78,45 +73,14 @@ class htmlCss extends React.Component {
                edges.node.data.category === selectedOption.value)
             .map((edges) => {
             return (
-
-              <Grid item xs={12} sm={6} md={4} style={{padding:24}}
-               key={edges.node.id}>
-              <Card >
-              <CardMedia style={{height:0, paddingTop:'56.25%'}}
-              image={edges.node.data.Thumbnail}
-              title={edges.node.data.Title}
-              />
-
-              <CardContent>
-                <Typography gutterBottom variant="inherit" component="h3" 
-                    style={{height:40,overflow:'hidden',fontSize:'18px'}}>
-                    {edges.node.data.Title}
-                </Typography>
-                <Typography component="p" style={{marginBottom:'10px'}} >
-                "{edges.node.data.Description.substr(0,100)} ..."
-                </Typography>
-                <Typography component="p">
-                {edges.node.data.Channel}
-                </Typography>
-                <Typography component="p">
-                {edges.node.data.category}
-                </Typography>
-                <Typography component="p">
-                {edges.node.data.PublishedDate}
-                </Typography>
-              </CardContent>
-              <CardActions>
-              <Button size="small" color="primary" href={edges.node.data.VideoURL}
-               target="_blank">
-                Go to Course
-               </Button>
-              </CardActions>
-              </Card>
-              </Grid>
+            
+              <PageGrid grids={edges} key={edges.node.id}/>
  
             )
         })}                   
         </Grid>
+
+
             
             </div>
             </Layout>
