bc7a8f2be305e0a2d032c2e3b9b5706293d80f70
IOT-Arduino <bunkajin@icloud.com>
2019-10-15 15:05:05 +0900

    refactor TutsBlock.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import Header from "../components/Header"
 import StyledHero from "../components/StyledHero"
 import SideBar1 from "../components/SideBar1"
 import SEO from '../components/SEO'
+import TutsBlock from '../components/TutsBlock'
 
 import styles from "./index.module.scss"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
@@ -147,159 +148,9 @@ const IndexPage = ({ data }) => {
           </div>
         </div>
 
-        <div className={styles.contentRegion1}>
-          <section className="row">
-            <div className="col-md-12">
-              <h2>Programing Tutorials</h2>
-              <p>Youtube,Udemy etc..</p>
-            </div>
-          </section>
-        </div>
+        <TutsBlock />
 
-        <div className={styles.blockWrapper}>
-          <section className="row">
-            <div className="col-sm-6 col-lg-4">
-              <div className={styles.block}>
-                <Link to="/tuts/reaGatsby">
-                  <FontAwesomeIcon
-                    icon={faReact}
-                    style={{ color: "#0FAF97", fontSize: "3rem" }}
-                  />
-                  <h3 className={styles.blockTitle}>
-                    React
-                    <br />
-                    Gatsby
-                  </h3>
-                </Link>
-                <p className={styles.blockText}>
-                  React&Gatsby Tutorial Information
-                </p>
-              </div>
-
-              <Link to="/tuts/reaGatsby">
-                <div className={styles.blockMask}>
-                  <div className={styles.blockCaption}>
-                    <FontAwesomeIcon icon={faArrowCircleRight} />
-                  </div>
-                </div>
-              </Link>
-            </div>
-
-            <div className="col-sm-6 col-lg-4">
-              <div className={styles.block}>
-                <Link to="/tuts/vuejs">
-                  <FontAwesomeIcon
-                    icon={faVuejs}
-                    style={{ color: "#0FAF97", fontSize: "3rem" }}
-                  />
-                  <h3>
-                    Vue.js
-                    <br />
-                    Gridsome
-                  </h3>
-                </Link>
-                <p>VueJs&Gridsome Tutorial Information</p>
-              </div>
-
-              <Link to="/tuts/vuejs">
-                <div className={styles.blockMask}>
-                  <div className={styles.blockCaption}>
-                    <FontAwesomeIcon icon={faArrowCircleRight} />
-                  </div>
-                </div>
-              </Link>
-            </div>
-
-            <div className="col-sm-6 col-lg-4">
-              <div className={styles.block}>
-                <Link to="/tuts/wordpress">
-                  <FontAwesomeIcon
-                    icon={faWordpress}
-                    style={{ color: "#0FAF97", fontSize: "3rem" }}
-                  />
-                  <h3>Wordpress</h3>
-                </Link>
-                <p>
-                  Opensource ContentsManagementSystem(CMS).Initial Version
-                  Released in Jul2003.
-                </p>
-              </div>
-
-              <Link to="/tuts/wordpress">
-                <div className={styles.blockMask}>
-                  <div className={styles.blockCaption}>
-                    <FontAwesomeIcon icon={faArrowCircleRight} />
-                  </div>
-                </div>
-              </Link>
-            </div>
-
-            <div className="col-sm-6 col-lg-4">
-              <div className={styles.block}>
-                <Link to="/tuts/javascript">
-                  <FontAwesomeIcon
-                    icon={faJsSquare}
-                    style={{ color: "#0FAF97", fontSize: "3rem" }}
-                  />
-                  <h3>JavaScript/jQuery</h3>
-                </Link>
-                <p>JavaScript&jQuery Tutorial Information</p>
-              </div>
-
-              <Link to="/tuts/javascript">
-                <div className={styles.blockMask}>
-                  <div className={styles.blockCaption}>
-                    <FontAwesomeIcon icon={faArrowCircleRight} />
-                  </div>
-                </div>
-              </Link>
-            </div>
-
-            <div className="col-sm-6 col-lg-4">
-              <div className={styles.block}>
-                <Link to="/tuts/htmlCss">
-                  <FontAwesomeIcon
-                    icon={faFileCode}
-                    style={{ color: "#0FAF97", fontSize: "3rem" }}
-                  />
-                  <h3>HTML/CSS</h3>
-                </Link>
-                <p>HTML&CSS Tutorial Information</p>
-              </div>
-
-              <Link to="/tuts/htmlCss">
-                <div className={styles.blockMask}>
-                  <div className={styles.blockCaption}>
-                    <FontAwesomeIcon icon={faArrowCircleRight} />
-                  </div>
-                </div>
-              </Link>
-            </div>
-
-            <div className="col-sm-6 col-lg-4">
-              <div className={styles.block}>
-                <Link to="/tuts/techNews">
-                  <FontAwesomeIcon
-                    icon={faDesktop}
-                    style={{ color: "#0FAF97", fontSize: "3rem" }}
-                  />
-                  <h3>TechNews</h3>
-                </Link>
-                <p>TechNews（To be Created）</p>
-              </div>
-
-              <Link to="/tuts/techNews">
-                <div className={styles.blockMask}>
-                  <div className={styles.blockCaption}>
-                    <FontAwesomeIcon icon={faArrowCircleRight} />
-                  </div>
-                </div>
-              </Link>
-            </div>
-          </section>
-        </div>
-
-        <section className={styles.contentRegion2}>
+              <section className={styles.contentRegion2}>
           <div className="row">
             <div className="col-md-5">
               <Image
